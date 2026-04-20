9cf76f76abdc8c1d1cdc335a83063e739d522881
David Foster <davidfoster.acc@gmail.com>
2026-04-19 23:11:10 -0400

    instantiated some modules in ARM_CPU

--- ARM_CPU.v
+++ ARM_CPU.v
@@ -11,12 +11,33 @@ module ARM_CPU (
 	output reg [63:0] PC
 );
 	reg [63:0] PC_to_INSTRUCT_MEM;
-	wire [63:0] PC_plus4;
-	wire [31:0] INSTRUCT_MEM_OUT;
-	wire [63:0] INSTRUCT_SIGN_EXT;
-	wire [3:0] ALU_CONTROL_OUT;
+	reg [63:0] PC_plus4;
+	reg [31:0] INSTRUCT_MEM_OUT;
+	reg [63:0] INSTRUCT_SIGN_EXT;
+	reg [3:0] ALU_CONTROL_OUT;
 	wire ALU_ZERO;
 	
-	
+
+endmodule
+
+module mux_64 (
+	input [63:0] in_1,
+	input [63:0] in_2,
+	input ctrl,
+	output [63:0] out
+);
+	// If control bit 0, pick first input
+	assign out = (ctrl == 0) ? in_1 : in_2;
+
+endmodule
+
+module mux_5 (
+	input [4:0] in_1,
+	input [4:0] in_2,
+	input ctrl,
+	output [4:0] out
+); 
+	// If control bit 0, pick first input
+	assign out = (ctrl == 0) ? in_1 : in_2;
 
 endmodule
